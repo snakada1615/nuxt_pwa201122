47886bb2cd4fc15c50fb167f7cfd5d4c582801c8
shunichi <khf04440@nifty.com>
2020-11-21 16:22:47 +0900

    1st commit

---
+++ pages/page02.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="page">
+    <div>おはようございます</div>
+    <label>
+     <span>
+       お名前:
+     </span>
+      <input
+        type="text"
+        v-model="user.name"
+      >
+    </label>
+    <label>
+     <span>
+       email:
+     </span>
+      <input
+        type="text"
+        v-model="user.email"
+      >
+    </label>
+    <button
+      type="button"
+      @click="sync"
+    >
+      Sync database
+    </button>
+    <div>
+      <b-table
+        striped
+        :items="items"
+        :fields="fields"
+        :current-page="currentPage"
+        :per-page="perPage"
+        :sort-by="sortBy"
+        :sort-desc="sortDesc"
+      ></b-table>
+      <b-form-group
+        label="Per page"
+        label-cols-sm="6"
+        label-cols-md="4"
+        label-cols-lg="3"
+        label-align-sm="right"
+        label-size="sm"
+        label-for="perPageSelect"
+        class="mb-0"
+      >
+        <b-form-select
+          v-model="perPage"
+          id="perPageSelect"
+          size="sm"
+          :options="pageOptions"
+        ></b-form-select>
+      </b-form-group>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="totalRows"
+        :per-page="perPage"
+        align="fill"
+        size="sm"
+        class="my-0"
+      ></b-pagination>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+  //  import PouchDB from '@/plugins/pouchdb'
+  import PouchDB from 'pouchdb'
+
+  var $ = require('jQuery');
+
+  export default {
+    data() {
+      return {
+        user: {
+          name: "",
+          email: ""
+        },
+        items: [],
+        fields: [
+          {key: 'Group', sortable: true, tdClass: 'd-none', thClass: 'd-none'},
+          {key: 'Name', sortable: true},
+          {key: 'En', sortable: true},
+          {key: 'Pr', sortable: true},
+          {key: 'Va', sortable: true},
+          {key: 'Fe', sortable: true},
+        ],
+        totalRows: 1,
+        currentPage: 1,
+        perPage: 5,
+        pageOptions: [5, 10, 15, {value: 100, text: "Show a lot"}],
+        sortBy: 'Name',
+        sortDesc: false,
+        filter: null,
+        filterOn: [],
+        mydat: [
+          {age: '40', first_name: 'Dickerson', last_name: 'Macdonald'},
+          {age: '21', first_name: 'Larsen', last_name: 'Shaw'},
+          {age: '89', first_name: 'Geneva', last_name: 'Wilson'},
+          {age: '38', first_name: 'Jami', last_name: 'Carney'}
+        ]
+      }
+    },
+    mounted() {
+      const fct = new PouchDB('@/assets/dbs/fct');
+      const vm = this;
+      fct.info().then(function (info) {
+        console.log(info);
+      })
+      fct.allDocs({include_docs: true})
+        .then(function (docs) {
+          $.each(docs.rows, function (index, val) {
+            vm.items.push({
+              'Group': val.doc.Food_grp,
+              'Name': val.doc.Food_name,
+              'En': val.doc.Energy,
+              'Pr': val.doc.Protein,
+              'Va': val.doc.VITA_RAE,
+              'Fe': val.doc.FE
+            })
+          })
+          // Set the initial number of items
+          vm.totalRows = vm.items.length
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    },
+    methods: {
+      sync() {
+        const dbs = ['fct', 'dri', 'pop', 'crop_national', 'crop_name']
+        // Replicating a local database to Remote
+        dbs.map(function (value, index, array) {
+          const localdb = new PouchDB('@/assets/dbs/' + value)
+          const remotedb = new PouchDB(
+            'http://snakada:Bluecity1965@localhost:5984/' + value
+          )
+          localdb
+            .sync(remotedb)
+            .on('complete', function () {
+              console.log(value + ': synced')
+            })
+            .on('error', function (err) {
+              console.log(err)
+            })
+        })
+      },
+      add() {
+        const db = firebase.firestore()
+        let dbUsers = db.collection('users').add({
+          name: this.user.name,
+          email: this.user.email,
+        })
+          .then(ref => {
+            this.user.name = ''
+            this.user.email = ''
+            console.log('added -> Add ID: ', ref.id)
+          })
+      },
+    },
+  }
+</script>
---
+++ plugins/pouchdb.js
@@ -0,0 +1,1 @@
+var PouchDB = require('pouchdb');
