682cefb70d40783a37ff7c45c094e697fe7f8d6a
shunichi <khf04440@nifty.com>
2021-09-23 05:49:42 +0300

    00920@ethiopia

--- pages/test5.vue
+++ pages/test5.vue
@@ -1,23 +1,8 @@
 <template>
   <b-container>
     <div>Hello world!</div>
-    <b-button variant="success" @click="$bvModal.show('test')">test</b-button>
-    <login-sms name="test" :uid.sync="uid"/>
-    <div>user: {{uid}}</div>
   </b-container>
 </template>
 
 <script>
-  import loginSms from "@/components/molecules/loginSms";
-
-  export default {
-    components:{
-      loginSms
-    },
-    data(){
-      return {
-        uid:'',
-      }
-    }
-  }
 </script>
